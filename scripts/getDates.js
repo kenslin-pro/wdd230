37cc506142795dfd483b0bb9f93dc7defd7844e4
kenslin-pro <kenslinerohine@gmail.com>
2023-07-13 22:28:51 +0300

    added changes

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,24 +1,36 @@
+// Get the current year
+var currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
 
-function updateCurrentYear() {
-  const currentYear = new Date().getFullYear();
-  const currentYearSpan = document.getElementById("currentYear");
-  currentYearSpan.textContent = currentYear;
-}
-function updateLastModified() {
-  const lastModified = new Date(document.lastModified).toLocaleDateString();
-  const lastModifiedParagraph = document.getElementById("lastModified");
-  lastModifiedParagraph.textContent = "Last modified: " + lastModified;
-}
+// Get the last modified date of the document
+var lastModifiedDate = new Date(document.lastModified);
+document.getElementById("lastModified").textContent = "Last modified: " + lastModifiedDate;
 
-updateCurrentYear();
-updateLastModified();
-document.addEventListener("DOMContentLoaded", function() {
-  let visitCount = localStorage.getItem("visitCount");
-  if (visitCount) {
-    visitCount = parseInt(visitCount) + 1;
-  } else {
-    visitCount = 1;
-  }
-  localStorage.setItem("visitCount", visitCount);
-  document.getElementById("visit-counter").textContent = visitCount + " visits";
+const hamburgerButton = document.getElementById('hamburgerButton');
+const mainMenu = document.getElementById('mainMenu');
+const darkModeToggle = document.getElementById('darkModeToggle');
+const mainContent = document.querySelector('main');
+
+// Hamburger button click event
+hamburgerButton.addEventListener('click', () => {
+  mainMenu.style.display = mainMenu.style.display === 'none' ? 'block' : 'none';
+});
+
+// Dark mode toggle button click event
+darkModeToggle.addEventListener('click', () => {
+  mainContent.classList.toggle('dark-mode');
+});
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
 });
