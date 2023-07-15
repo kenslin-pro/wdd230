02b9d86a854846b2d16feca22ba8ef55f866dc9f
kenslin-pro <kenslinerohine@gmail.com>
2023-07-15 21:31:06 +0300

    added chnges

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -37,6 +37,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
     address.textContent = member.address;
     memberCard.appendChild(address);
 
+    const phone = document.createElement('p');
+    address.textContent = member.phone;
+    memberCard.appendChild(phone);
+
+    const website = document.createElement('p');
+    address.textContent = member.website;
+    memberCard.appendChild(website);
+
+    const membershipLevel = document.createElement('p');
+    address.textContent = member.membershipLevel;
+    memberCard.appendChild(membershipLevel);
+
+
     // Add more member details as needed
 
     membersContainer.appendChild(memberCard);
@@ -54,6 +67,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
     address.textContent = member.address;
     memberItem.appendChild(address);
 
+    const phone = document.createElement('p');
+    address.textContent = member.phone;
+    memberItem.appendChild(phone);
+
+    const website = document.createElement('p');
+    address.textContent = member.website;
+    memberItem.appendChild(website);
+
+    const membershipLevel = document.createElement('p');
+    address.textContent = member.membershipLevel;
+    memberItem.appendChild(membershipLevel);
+
+
     // Add more member details as needed
 
     membersContainer.appendChild(memberItem);
@@ -62,3 +88,21 @@ window.addEventListener('DOMContentLoaded', (event) => {
   // Initially, load the grid view
   toggleView('grid');
 });
+
+//sidebar
+// JavaScript code
+const sidebarMessage = document.getElementById('sidebar-message');
+const lastVisitDate = localStorage.getItem('lastVisitDate');
+
+if (lastVisitDate) {
+  const daysBetween = Math.floor((Date.now() - lastVisitDate) / (1000 * 60 * 60 * 24));
+  
+  if (daysBetween === 0) {
+    sidebarMessage.textContent = "Back so soon! Awesome!";
+  } else {
+    const pluralSuffix = daysBetween === 1 ? "" : "s";
+    sidebarMessage.textContent = `You last visited ${daysBetween} day${pluralSuffix} ago.`;
+  }
+}
+
+localStorage.setItem('lastVisitDate', Date.now());
