a135b0f6b671ef7c6528ed093e1cfe0ababfd6ff
kenslin-pro <kenslinerohine@gmail.com>
2023-07-19 02:47:28 +0300

    added change

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,4 +1,24 @@
+const apiKey = 'ff9f2ec82564c42998e466a10a7dbb7a';
+const city = 'Nairobi';
 
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+  .then(response => response.json())
+  .then(data => {
+    // Process the weather data and update the information card
+    const temperature = Math.round(data.main.temp - 273.15); // Convert from Kelvin to Celsius
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
+
+    // Update the information card with the weather data
+    document.getElementById('temperature').textContent = `${temperature}°C`;
+    document.getElementById('description').textContent = description;
+    document.getElementById('weather-icon').setAttribute('src', `https://openweathermap.org/img/w/${icon}.png`);
+  })
+  .catch(error => {
+    console.log('Error fetching weather data:', error);
+  });
+
+//weather
 
 window.addEventListener('DOMContentLoaded', (event) => {
   const membersContainer = document.getElementById('members-container');
